83ccb60f2db03a904a6080f3bc3ba97defd1546e
Landon Fuhr <landonfuhr@gmail.com>
2021-07-08 10:43:57 -0600

    Refactor github icon

--- src/frontend/src/pages/home/ButtonsBar/ButtonsImpl.tsx
+++ src/frontend/src/pages/home/ButtonsBar/ButtonsImpl.tsx
@@ -20,11 +20,7 @@ export const GithubButton = () => {
       onClick={() => openExternalLink("https://github.com/LandonFuhr/aseen")}
       text="Source Code"
       icon={
-        <img
-          className="github-icon"
-          src="GitHub-Mark-120px-plus.png"
-          alt="Github Logo"
-        />
+        <img className="github-icon" src="github_logo.png" alt="Github Logo" />
       }
     />
   );
